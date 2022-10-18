9520f95bff31bf0d5e0bdd5a2d92753b46ef52fc
gutuJosh <horwath_cosmin@yahoo.com>
2022-10-18 15:00:32 +0200

    fix bug in CustomBtn

--- src/components/customBtn.js
+++ src/components/customBtn.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function CustomBtn(props) {
   const lable = navigator.onLine
-    ? `Tap for a text note | Tap &amp; hold for a voice note`
+    ? `Tap for a text note | Tap & hold for a voice note`
     : `Tap for a text note`;
+
+  const element = useRef(null);
+  const [loaded, setLoaded] = useState(false);
+
+  const addAnimation = () => {
+    element.current.classList.add("animated");
+    setLoaded(true);
+  };
+
+  useEffect(() => {
+    if (!loaded) {
+      element.current.addEventListener("transitionend", addAnimation);
+    }
+    return () => {
+      try {
+        element.current.removeEventListener("transitionend", addAnimation);
+      } catch (e) {
+        //
+      }
+    };
+  }, []);
+
   return (
-    <div {...props} title={lable}>
-      <span className="title-default"> + {props.title}</span>
-      <span className="title-clicked"> Save</span>
-      <span className="title-audio-end"> Stop</span>
-    </div>
+    <footer>
+      <div {...props} title={lable} ref={element}>
+        <span className="title-default"> + {props.title}</span>
+        <span className="title-clicked"> Save</span>
+        <span className="title-audio-end">
+          <svg className="icn">
+            <use href="#microphone-icon"></use>
+          </svg>
+        </span>
+      </div>
+    </footer>
   );
 }
 
