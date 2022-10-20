c0ae2885f6a36add5dc93f6a2cf6c3362cbef38e
gutuJosh <horwath_cosmin@yahoo.com>
2022-10-20 17:29:35 +0200

    add item details styles

--- src/components/itemDetails.js
+++ src/components/itemDetails.js
@@ -18,9 +18,10 @@ function ItemDetails(props) {
   }, [props.data]);
 
   return (
-    <div className="item-details">
-      <label>
-        Items number:
+    <div className="item-details flex wrap">
+      <label className="flex-item auto pad-x-10">
+        <span className="inline-block">Items number:</span>
+
         <input
           type="text"
           value={qty}
@@ -28,8 +29,9 @@ function ItemDetails(props) {
           onClick={(e) => e.stopPropagation()}
         />
       </label>
-      <label>
-        Price per item:
+      <label className="flex-item auto pad-x-10">
+        <span className="inline-block">Price per item:</span>
+
         <input
           type="text"
           value={price}
@@ -37,23 +39,27 @@ function ItemDetails(props) {
           onClick={(e) => e.stopPropagation()}
         />
       </label>
-      <button
-        onClick={(e) => {
-          props.update(props.data.name, {
-            id: props.data.id,
-            name: props.data.name,
-            qty: qty,
-            price: price,
-            status: props.data.status,
-          });
-          setTimeout(
-            () => e.target.parentNode.closest("li").classList.remove("active"),
-            300
-          );
-        }}
-      >
-        Ok
-      </button>
+      <label className="pad-x-10">
+        <button
+          className="pad-x-20"
+          onClick={(e) => {
+            props.update(props.data.name, {
+              id: props.data.id,
+              name: props.data.name,
+              qty: qty,
+              price: price,
+              status: props.data.status,
+            });
+            setTimeout(
+              () =>
+                e.target.parentNode.closest("li").classList.remove("active"),
+              300
+            );
+          }}
+        >
+          OK
+        </button>
+      </label>
     </div>
   );
 }
